2a71c612c2eaf4027874eef7be1793dc931b5c7c
Sai-222777 <122385607+Sai-222777@users.noreply.github.com>
2025-09-10 16:39:25 +0530

    Update lukevassallo_xor_cipher.v

--- src/lukevassallo_xor_cipher.v
+++ src/lukevassallo_xor_cipher.v
@@ -13,8 +13,15 @@
 // limitations under the License.
 
 module tt_um_lukevassallo_xor_cipher (
-input [7:0] io_in,
-output [7:0] io_out );
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
 
 //xor_cipher uut (
 //    .clk(io_in[0]),
