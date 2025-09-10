46ae004f378c4bbfad28c036d13d260edd0c567b
Sai-222777 <122385607+Sai-222777@users.noreply.github.com>
2025-09-10 16:45:25 +0530

    Update lukevassallo_xor_cipher.v

--- src/lukevassallo_xor_cipher.v
+++ src/lukevassallo_xor_cipher.v
@@ -24,37 +24,37 @@ module tt_um_lukevassallo_xor_cipher (
 );
 
 //xor_cipher uut (
-//    .clk(io_in[0]),
-//    .rst(io_in[1]),
+//    .clk(ui_in[0]),
+//    .rst(ui_in[1]),
   
-//    .data_stream(io_in[2]),
-//    .external_k(io_in[3]),
+//    .data_stream(ui_in[2]),
+//    .external_k(ui_in[3]),
     
-//    .e(io_out[0]),
-//    .d(io_out[1]),
+//    .e(uo_out[0]),
+//    .d(uo_out[1]),
     
-//    .cfg_en(io_in[4]),
-//    .cfg_i(io_in[5]),
-//    .en(io_in[6]),
-//    .cfg_o(io_out[2]),
-//    .heartbeat(io_out[7:3])
+//    .cfg_en(ui_in[4]),
+//    .cfg_i(ui_in[5]),
+//    .en(ui_in[6]),
+//    .cfg_o(uo_out[2]),
+//    .heartbeat(uo_out[7:3])
 //);
 dual_xor_stream_cipher #( .M(32) ) uut  (
-    .clk(io_in[0]),
-    .rst(io_in[1]),
-    .tx_p(io_in[2]),
-    .rx_e(io_in[3]),
-    .cfg_en(io_in[4]),
-    .cfg_i(io_in[5]),
-    .tx_en(io_in[6]),
-    .rx_en(io_in[7]),
+    .clk(ui_in[0]),
+    .rst(ui_in[1]),
+    .tx_p(ui_in[2]),
+    .rx_e(ui_in[3]),
+    .cfg_en(ui_in[4]),
+    .cfg_i(ui_in[5]),
+    .tx_en(ui_in[6]),
+    .rx_en(ui_in[7]),
     
-    .tx_e(io_out[0]),
-    .rx_p(io_out[1]),
-    .dbg_tx_p(io_out[2]),
-    .dbg_rx_e(io_out[3]), 
+    .tx_e(uo_out[0]),
+    .rx_p(uo_out[1]),
+    .dbg_tx_p(uo_out[2]),
+    .dbg_rx_e(uo_out[3]), 
 
-    .cfg_o(io_out[4]),
-    .heartbeat(io_out[7:5])
+    .cfg_o(uo_out[4]),
+    .heartbeat(uo_out[7:5])
 );
 endmodule
