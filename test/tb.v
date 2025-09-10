444ea0c94ebb727cbd25a082d0a3ba24fe7f8491
Sai-222777 <122385607+Sai-222777@users.noreply.github.com>
2025-09-10 20:14:19 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_lukevassallo_xor_cipher (
+  tt_um_lukevassallo_xor_cipher uut (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
